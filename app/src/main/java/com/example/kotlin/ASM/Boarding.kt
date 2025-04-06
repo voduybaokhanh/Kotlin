47526d7dbea91aacc28614e89c5791c40a209879
Phong908 <122242560+khanhvo908@users.noreply.github.com>
2025-04-06 12:26:49 +0700

    Update Boarding.kt

--- app/src/main/java/com/example/kotlin/ASM/Boarding.kt
+++ app/src/main/java/com/example/kotlin/ASM/Boarding.kt
@@ -16,9 +16,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.kotlin.R
 
 @Composable
@@ -89,4 +91,10 @@ fun BoardingScreen(navController: NavController) {
             }
         }
     }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun BoardingScreenPreview() {
+    BoardingScreen(navController = rememberNavController())
 }
